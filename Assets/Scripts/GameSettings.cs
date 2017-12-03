dc0f424ca7e688a973b1e0c26548b5b7ba6d7063
JuanCarlosGdM <juancarlosgdm@gmail.com>
2017-12-04 00:44:28 +0100

    Traps configuration added & some animations

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -9,6 +9,10 @@ public class GameSettings : MonoBehaviour {
     public float keyboardCD = 2.0f;
     public int limitKeyboardSteps = 3;
 
+    [Header("Traps Settings")]
+    public int spikesProb = 1;
+    public int boulderProb = 1;
+
     private void Awake() {
         // Singleton
         if (instance == null) {
